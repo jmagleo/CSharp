4d411f9a85551c3fd4823b16f4379a03b121763f
Magleo <justin.a.m.magleo@accenture.com>
2017-06-30 15:56:40 +0800

    Add project files.

---
+++ Calculator/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Class1
+    {
+        public int numOne
+        {
+            get
+            {
+                return this.numOne;
+            }
+
+            set
+            {
+                this.numOne = value;
+            }
+        }
+
+        public int numTwo
+        {
+            get
+            {
+                return this.numTwo;
+            }
+            set
+            {
+                this.numOne = value;
+            }
+
+        }
+
+        void Numbers()
+        {
+            this.numOne = Convert.ToInt32(Console.ReadLine());
+            this.numTwo = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[2];
+            int result = 0;
+            string operation = "";
+            string exit = "";
+
+            Console.WriteLine("Basic Calculator");
+            Console.WriteLine("Operations available: +, -, *, /");
+            
+            while (exit != "No")
+            {
+
+            
+            Console.WriteLine("Enter Number: ");
+            arr[0] = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Operation: ");
+            operation = Console.ReadLine();
+
+            Console.WriteLine("Enter 2nd number: ");
+            arr[1] = Convert.ToInt32(Console.ReadLine());
+
+            if (operation == "+")
+            {
+                result = arr[0] + arr[1];
+            }
+
+            else if (operation == "-")
+            {
+                result = arr[0] - arr[1];
+            }
+
+            else if (operation == "*")
+            {
+                result = arr[0] * arr[1];
+            }
+
+            else if (operation == "/")
+            {
+                result = arr[0] / arr[1];
+            }
+
+            else
+            {
+                Console.WriteLine("This is not supported");
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine("Continue? Yes or No.");
+            exit = Console.ReadLine();
+
+            }
+            Console.Read();
+            
+        }
+    }
+}
